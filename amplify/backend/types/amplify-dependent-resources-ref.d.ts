a398226896b007e1d94f62069bb4f6b9d76df69b
Ikeda <askayu@b0be837c4567.ant.amazon.com>
2023-03-10 16:09:54 +0900

    api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "notesapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "reactamplifytestcf8babfd": {
       "AppClientID": "string",
